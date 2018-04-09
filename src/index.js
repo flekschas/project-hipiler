29d02f685695fb925eb5a92c78ed52bbf125560d
Fritz Lekschas <service@lekschas.de>
2018-04-09 14:26:39 -0400

    Add demo link. Fix max content width

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import styles from './index.scss';  // eslint-disable-line
 
 mediumZoom('.zoomable', { margin: 48 });
 
+const figTeaser = document.getElementById('fig-teaser');
+const demoLink = document.getElementById('demo-link');
+const figTeaserClass = figTeaser.getAttribute('class');
+
+demoLink.addEventListener('mouseenter', () => {
+  figTeaser.className = `${figTeaserClass} active`;
+});
+demoLink.addEventListener('mouseleave', () => {
+  figTeaser.className = figTeaserClass;
+});
+
 setTimeout(() => {
-  document.getElementById('fig-teaser').play();
+  figTeaser.play();
 }, 5000);
